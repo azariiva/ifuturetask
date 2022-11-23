2ec6a60fc4150fba2813bbc0cb4a72e0a2647f01
Iskander Zaripov <zar.isk.rus@gmail.com>
2022-11-23 19:06:05 +0500

    server validation

--- server/src/main/java/ru/spbifuture/account/server/controller/AccountAPI.java
+++ server/src/main/java/ru/spbifuture/account/server/controller/AccountAPI.java
@@ -1,18 +1,20 @@
 package ru.spbifuture.account.server.controller;
 
-import com.sun.istack.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @RequestMapping("/api/account")
 public interface AccountAPI {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    Long getAmount(@PathVariable("id") @NotNull Integer id);
+    Long getAmount(@PathVariable("id") @NotNull @Min(1) Integer id);
 
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void addAmount(@PathVariable("id") @NotNull Integer id,
-                   @RequestParam("value") @NotNull Long value);
+    void addAmount(@PathVariable("id") @NotNull @Min(1) Integer id,
+                   @RequestParam("value") @NotNull @Min(1)  Long value);
 }
--- server/src/main/java/ru/spbifuture/account/server/controller/AccountRestAPI.java
+++ server/src/main/java/ru/spbifuture/account/server/controller/AccountRestAPI.java
@@ -1,10 +1,16 @@
 package ru.spbifuture.account.server.controller;
 
-import com.sun.istack.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.spbifuture.account.server.businesslogic.AccountService;
 
+import javax.validation.ConstraintViolationException;
+
 @RestController
+@Validated
 public class AccountRestAPI implements AccountAPI {
 
     private final AccountService accountService;
@@ -13,13 +19,19 @@ public class AccountRestAPI implements AccountAPI {
         this.accountService = accountService;
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String constraintViolationExceptionHandler(ConstraintViolationException exception) {
+        return exception.getMessage();
+    }
+
     @Override
     public Long getAmount(Integer id) {
         return accountService.getAmount(id);
     }
 
     @Override
-    public void addAmount(@NotNull Integer id, @NotNull Long value) {
+    public void addAmount(Integer id, Long value) {
         accountService.addAmount(id, value);
     }
 }
